0b95444d43aa1a2183d5d92dcfc6685dfb38d3bd
amit123ak <41484488+amit123ak@users.noreply.github.com>
2025-08-31 23:29:56 +0530

    Create RotateRightLinkList.java

---
+++ RotateRightLinkList.java
@@ -0,0 +1,104 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class Node{
+     int  data;
+     Node next;
+    
+    Node(int data)
+    {
+        
+        this.data=data;
+        this.next=null;
+    }
+ public static void findMiddle(Node node)
+    {
+        Node head =node;
+        Node temp=node;
+        int len=0;
+        while(head!=null)
+        {
+           head= head.next;
+           len++;
+        }
+       System.out.println("\\n");
+        int midlen=len/2;
+        
+        while(midlen>0)
+        {
+            temp=temp.next;
+            midlen--;
+            
+        }
+        
+        System.out.println("mid is "+ temp.data);
+        
+        
+    }
+    
+    public static Node Rotate(Node node,int k)
+    {
+        Node temp =node;
+        Node head=node;
+        int n=1;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+            n++;
+        }
+        System.out.println(n+ "  "+ temp.data);
+        k=k%n;
+        temp.next=head;
+         //n=6;
+          temp=node;
+         while(n-k>1)
+         {
+             temp=temp.next;
+        System.out.println("second. "+ temp.data);
+
+             n--;
+         }
+         head=temp.next;
+       temp.next=null;
+         
+         
+        
+   
+    return head;
+    
+    }
+    
+    
+    public static void displayed(Node node)
+    {
+       Node temp=node;
+        while(temp!=null)
+        {
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+            
+        }
+        System.out.print("NULL");
+    }
+    
+    
+     public static void main(String[] args)
+     {
+        Node node=new Node(10);
+        node.next=new Node(30);
+        node.next.next=new Node(40);
+        node.next.next.next=new Node(50);
+        node.next.next.next.next=new Node(60);
+        node.next.next.next.next.next=new Node(70);
+
+        displayed(node);
+        findMiddle(node);
+      Node reHead=  Rotate(node,2);
+      System.out.println("displayed rotate node");
+      
+              displayed(reHead);
+
+         
+     }
+    
+}
+
