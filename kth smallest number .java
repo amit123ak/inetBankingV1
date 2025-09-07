d93b43a66e3427481be9231b24ee984ea6f3e41b
amit123ak <41484488+amit123ak@users.noreply.github.com>
2025-09-07 21:55:46 +0530

    Create kth smallest number .java

---
+++ kth smallest number .java
@@ -0,0 +1,20 @@
+mport java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        
+        int arr[] = {3, 4, 2, 7, 3, 9, 10};
+        int k = 4;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i = 0; i < arr.length; i++) {
+            pq.add(arr[i]);
+            if(pq.size() >k) {
+                pq.poll(); // remove smallest element if size exceeds k
+            }
+        }
+        
+        System.out.println(k + "-th smallest element is: " + pq.peek());
+    }
+}
